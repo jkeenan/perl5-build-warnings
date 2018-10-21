968f8931a29f0ca058b5d7781d958e1485eaa170
James E Keenan <jkeenan@cpan.org>
2018-10-20 20:53:16 -0400

    CPAN version 0.01
    Add metadata files. Update README.

---
+++ .version_info.pl
@@ -0,0 +1,8 @@
+%trans = (
+    version => { old => '0.01', new => '0.01' },
+    date    => { old => 'October 20 2018', new => 'October 20 2018' },
+    copyright => { old => '2018', new => '2018' },
+);
+
+1;
+
