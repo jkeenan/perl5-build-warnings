ca58532389662a9548a0f62f4fd799b9da4fa973
James E Keenan <jkeenan@cpan.org>
2021-09-10 08:17:07 -0400

    CPAN version 0.05
    Update $VERSION and dates.

--- .version_info.pl
+++ .version_info.pl
@@ -1,7 +1,7 @@
 %trans = (
-    version => { old => '0.03', new => '0.04' },
-    date    => { old => 'October 31 2018', new => 'December 05 2019' },
-    copyright => { old => '2018', new => '2019' },
+    version => { old => '0.04', new => '0.05' },
+    date    => { old => 'December 05 2019', new => 'September 10 2021' },
+    copyright => { old => '2019', new => '2021' },
 );
 
 1;
