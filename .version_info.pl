0648598075ce2b8bcad3e221c687829170fa431b
James E Keenan <jkeenan@cpan.org>
2026-07-03 15:57:00 -0400

    CPAN release 0.07
    Failed to run 'make manifest' before releasing 0.06 meant that new test
    data file was not found in tarball.

--- .version_info.pl
+++ .version_info.pl
@@ -1,7 +1,7 @@
 %trans = (
-    version => { old => '0.05', new => '0.06' },
-    date    => { old => 'September 10 2021', new => 'July 3 2026' },
-    copyright => { old => '2021', new => '2026' },
+    version => { old => '0.06', new => '0.07' },
+    date    => { old => 'July 3 2026', new => 'July 3 2026' },
+    copyright => { old => '2026', new => '2026' },
 );
 
 1;
